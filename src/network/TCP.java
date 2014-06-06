0b5ef5e2218b0c81201515388f15ef6492b6bd68
hiquen <quentin.hibon@polytechnique.edu>
2014-06-06 16:26:51 +0200

    add listener

--- src/network/TCP.java
+++ src/network/TCP.java
@@ -13,21 +13,23 @@ import java.nio.channels.SocketChannel;
 public class TCP {
 	ServerSocketChannel serverSocket;
 	SocketChannel clientSocket;
+	int port = 12347;
 	
 	public TCP(String ip, int port) {
-		connectClient(ip,port);
+		connectClient(ip, port);
 	}
-	
+
+	// For client: connect to server
 	public void connectClient(String ip, int port) {
 		try {
-			InetSocketAddress dest = new InetSocketAddress(ip,port);
+			InetSocketAddress dest = new InetSocketAddress(ip, port);
 			clientSocket = SocketChannel.open();
 			clientSocket.connect(dest);
 		} catch (IOException e) {
-			System.out.println("ERREUR connectClient : "+ip+":"+port);
+			System.out.println("ERREUR connectClient : " + ip + ":" + port);
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	// For Client
@@ -42,24 +44,44 @@ public class TCP {
 			fisC.close();
 			fis.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("Ficher "+path+"non trouvé !");
+			System.out.println("Ficher " + path + "non trouvé !");
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	/**
+	 * For server Listens on the port to create a new connection to a client who
+	 * has a task to share.
+	 */
+	public void listen() {
+		try {
+			ServerSocketChannel serverSocket = ServerSocketChannel.open();
+			InetSocketAddress local = new InetSocketAddress(port);
+			serverSocket.bind(local);
+			this.clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	// For Client
-	public void sendSerializedClass(String path) {}
-	
+	public void sendSerializedClass(String path) {
+	}
+
+	// For Client and Server
+
 	// For Client & Servers
-	public void ack() {}
-	
+	public void ack() {
+	}
+
 	// For Servers
-	public void sendResult() {}
-	
+	public void sendResult() {
+	}
+
 	// For Clients
 	public void endOfTask() {
-		
+
 	}
-	
+
 }
