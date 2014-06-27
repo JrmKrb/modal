4ec73ec158136be016ef9dbda75c43c6fa937b99
hiqua <quentin.hibon@polytechnique.edu>
2014-06-27 17:06:56 +0200

    add getClass method

--- src/network/Message.java
+++ src/network/Message.java
@@ -43,5 +43,18 @@ public class Message {
 		}
 		return null;
 	}
+	
+	public static Class<?> getClass(Socket s, int length){
+			byte[] bf = new byte[length];
+			try {
+				s.getInputStream().read(bf);
+				NetworkClassLoader classLoader = (NetworkClassLoader) Message.class.getClassLoader();
+				return classLoader.load(bf);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+	}
 
 }
