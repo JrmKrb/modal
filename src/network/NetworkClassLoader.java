60ac15440e78f95769e0346fce69d4e18126cd7f
hiqua <quentin.hibon@polytechnique.edu>
2014-06-27 17:04:12 +0200

    fix type argument

--- src/network/NetworkClassLoader.java
+++ src/network/NetworkClassLoader.java
@@ -1,7 +1,7 @@
 package network;
 
 class NetworkClassLoader extends ClassLoader {
-	public Class load(byte[] b) {
+	public Class<?> load(byte[] b) {
 		//we do not know the name of the class, hence "null" as a first argument
 		return defineClass(null, b, 0, b.length);
 	}
