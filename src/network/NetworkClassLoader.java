300b8d87d74526dd6068e3bff507191e727ff3ab
hiqua <quentin.hibon@polytechnique.edu>
2014-06-28 10:56:58 +0200

    fix (add) the constructor of NetworkClassLoader

--- src/network/NetworkClassLoader.java
+++ src/network/NetworkClassLoader.java
@@ -1,6 +1,11 @@
 package network;
 
+/* defineClass is protected, that's why we have to create our own custom class. */
 class NetworkClassLoader extends ClassLoader {
+	public NetworkClassLoader(ClassLoader parent){
+		super(parent);
+	}
+	
 	public Class<?> load(byte[] b) {
 		//we do not know the name of the class, hence "null" as a first argument
 		return defineClass(null, b, 0, b.length);
